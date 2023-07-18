bf2c20270f95f03cf1a1b39723856bf7aa7448af
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-07-18 11:21:14 +0700

    menggunakan js kamera

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PresensiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,7 @@ Route::middleware(['guest:karyawan'])->group(function () {
 Route::middleware(['auth:karyawan'])->group(function() {
     Route::get('/dashboard',[DashboardController::class, 'index']);
     Route::get('/prosesLogout', [AuthController::class, 'proseslogout']);
+
+    //Presensi
+    Route::get('/presensi/create',[PresensiController::class, 'create']);
 });
