ac5f92598f7bb594e4546ea968bb66b79768dfb1
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-08-04 17:17:25 +0700

    membuat rekap kehadiran

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,15 +15,30 @@ class DashboardController extends Controller
         $tahunIni = date("Y"); // 2023
         $nik = Auth::guard('karyawan')->user()->nik;
         $presensiHariIni = DB::table('presensi')->where('nik', $nik)->where('tgl_presensi', $hariIni)->first();
-        $historyBulanIni = DB::table('presensi')->whereRaw('MONTH(tgl_presensi)="'. $bulanIni . '"')
+        $historyBulanIni = DB::table('presensi')
+        ->whereRaw('nik', $nik)
+        ->whereRaw('MONTH(tgl_presensi)="'. $bulanIni . '"')
         ->whereRaw('YEAR(tgl_presensi)="' .$tahunIni . '"')
         ->orderBy('tgl_presensi')
         ->get();
 
+        $rekapPresensi = DB::table('presensi')
+        ->selectRaw('COUNT(nik) as jmlhadir, SUM(IF(jam_in > "07.00", 1, 0)) as jmlterlambat' )
+        ->where('nik', $nik)
+        ->whereRaw('MONTH(tgl_presensi)="'. $bulanIni . '"')
+        ->whereRaw('YEAR(tgl_presensi)="' .$tahunIni . '"')
+        ->first();
+
+        $leaderBoard = DB::table('presensi')
+        ->join('karyawan', 'presensi.nik', '=', 'karyawan.nik')
+        ->where('tgl_presensi', $hariIni )
+        ->orderBy('jam_in')
+        ->get();
+        
         $namaBulan = ["","Januari","Februari", "Maret", "April", "Mei","Juni","Juli", "Agustus", "September","Oktober", "November","Desember"];
 
         // dd($historyBulanIni); buat cek nanti
-        return view('dashboard.dashboard', compact('presensiHariIni','historyBulanIni', 'namaBulan', 'bulanIni','tahunIni'));
+        return view('dashboard.dashboard', compact('presensiHariIni','historyBulanIni', 'namaBulan', 'bulanIni','tahunIni','rekapPresensi', 'leaderBoard'));
         
     }
 }
